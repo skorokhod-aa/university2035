99946c3aaa2600b18808863d263e92ea9af873bd
skorohodaa <123@mail.ru>
2023-06-13 22:26:30 +0700

    fst commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+
+# s = aaaabcca
+
+# a: 5, b: 1, c: 2
+
+
+
+# def strcounter(s):
+#     for sym in s:
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+#
+# s = 'cccccc' N ** 2
+
+
+# def strcounter(s):
+#     for sym in set(s):
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+# s = 'aaabc'
+# # O(N) = M * N
+# # N * N - худший случай, если все символы уникальны
+# strcounter(s)
+
+
+
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    print(syms_counter)
+
+#O(N) = N
+
+strcounter('aaaabca')
